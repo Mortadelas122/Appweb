314a5905cd8667c82b2ff0629a4ba28116898d85
Alvaro LDSW <86538013+Mortadelas122@users.noreply.github.com>
2025-05-07 11:05:14 -0600

    Update server.js

--- server.js
+++ server.js
@@ -56,5 +56,4 @@ app.delete('/records/:id', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`));
